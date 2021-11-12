89da02d21c3e0156af99de0096b59f6b3d506f3a
dpuglielli <dpuglielli@flexion.us>
2021-11-12 16:31:32 -0600

    #3 dwd-ui-monetary-lab-3 16-more-tests

--- DWD.UI.Monetary/DWD.UI.Monetary.Tests/BasePeriodUseCaseTests.cs
+++ DWD.UI.Monetary/DWD.UI.Monetary.Tests/BasePeriodUseCaseTests.cs
@@ -1,6 +1,8 @@
 namespace DWD.UI.Monetary.Tests
 {
     using System;
+    using System.Linq;
+    using Domain.BusinessEntities;
     using Domain.UseCases;
     using Xunit;
 
@@ -23,6 +25,31 @@ namespace DWD.UI.Monetary.Tests
             {
                 Assert.NotNull(basePeriod);
             }
+        }
+
+        [Fact]
+        public void CalculateAltBasePeriodFromInitialClaimDateFail()
+        {
+            // Get base period from date
+            var myClaimDate = new DateTime(2021, 1, 02);
+            var basePeriodUseCase = new CalculateBasePeriod();
+            var result = basePeriodUseCase.CalculateBasePeriodFromInitialClaimDate(myClaimDate);
+            // Check result
+            Assert.NotNull(result);
+            var quarters = result.BasePeriodQuarters;
+            Assert.NotNull(quarters);
+            var actualQuarters = quarters.OrderBy(q => q.Year)
+                .ThenBy(q => q.QuarterNumber)
+                .ToArray();
+            var testQuarters = new UIQuarter[4];
+            testQuarters[0] = new UIQuarter(2020, 1);
+            testQuarters[1] = new UIQuarter(2020, 2);
+            testQuarters[2] = new UIQuarter(2020, 3);
+            testQuarters[3] = new UIQuarter(2020, 4);
+            Assert.Equal(testQuarters[0], actualQuarters[0]);
+            Assert.Equal(testQuarters[1], actualQuarters[1]);
+            Assert.Equal(testQuarters[2], actualQuarters[2]);
+            Assert.Equal(testQuarters[3], actualQuarters[3]);
         }
 
     }
