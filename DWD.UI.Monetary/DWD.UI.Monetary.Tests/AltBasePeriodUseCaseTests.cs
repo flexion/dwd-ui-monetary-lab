8ca4847d955df861478490b40cc91618683e893a
dpuglielli <dpuglielli@flexion.us>
2021-11-12 16:17:55 -0600

    fix #7 dwd-ui-monetary-lab-7 Implement alternate base period (story 16)
    created failing test

--- DWD.UI.Monetary/DWD.UI.Monetary.Tests/AltBasePeriodUseCaseTests.cs
+++ DWD.UI.Monetary/DWD.UI.Monetary.Tests/AltBasePeriodUseCaseTests.cs
@@ -38,5 +38,33 @@ namespace DWD.UI.Monetary.Tests
 
         }
 
+        [Fact]
+        public void CalculateAltBasePeriodFromInitialClaimDateFail()
+        {
+            // Get base period from date
+            var myClaimDate = new DateTime(2021, 1, 02);
+            var basePeriodUseCase = new CalculateBasePeriod();
+            var result = basePeriodUseCase.CalculateBasePeriodFromInitialClaimDate(myClaimDate);
+
+            // Check result
+            Assert.NotNull(result);
+            var quarters = result.AltBasePeriodQuarters;
+            Assert.NotNull(quarters);
+
+            var actualQuarters = quarters.OrderBy(q => q.Year)
+                .ThenBy(q => q.QuarterNumber)
+                .ToArray();
+            var testQuarters = new UIQuarter[4];
+            testQuarters[0] = new UIQuarter(2020, 2);
+            testQuarters[1] = new UIQuarter(2020, 3);
+            testQuarters[2] = new UIQuarter(2020, 4);
+            testQuarters[3] = new UIQuarter(2021, 1);
+
+            Assert.Equal(testQuarters[0], actualQuarters[0]);
+            Assert.Equal(testQuarters[1], actualQuarters[1]);
+            Assert.Equal(testQuarters[2], actualQuarters[2]);
+            Assert.Equal(testQuarters[3], actualQuarters[3]);
+
+        }
     }
 }
