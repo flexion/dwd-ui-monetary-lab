a85607229540303666980b47dd980c0c89b8a0bc
Jerry O''Brien <jerry.obrien@nelnet.net>
2021-11-22 14:39:27 -0600

    fixes #31 MonetaryController Updates

--- DWD.UI.Monetary/DWD.UI.Monetary.Service/Controllers/BasePeriodController.cs
+++ DWD.UI.Monetary/DWD.UI.Monetary.Service/Controllers/BasePeriodController.cs
@@ -8,21 +8,20 @@ namespace DWD.UI.Monetary.Service.Controllers
     using Microsoft.Extensions.Logging;
     using DWD.UI.Monetary.Domain.UseCases;
     using DWD.UI.Monetary.Service.Mappers;
-    using Microsoft.OpenApi.Extensions;
     using Models;
     using Swashbuckle.AspNetCore.Annotations;
 
     /// <summary>
-    /// Provides endpoints for monetary.
+    /// Provides endpoints for BasePeriod.
     /// </summary>
     [ApiController]
     [Route("[controller]")]
-    public class MonetaryController : ControllerBase
+    public class BasePeriodController : ControllerBase
     {
         /// <summary>
         /// Local logger reference.
         /// </summary>
-        private readonly ILogger<MonetaryController> logger;
+        private readonly ILogger<BasePeriodController> logger;
 
         /// <summary>
         /// Local reference to domain logic.
@@ -34,14 +33,14 @@ namespace DWD.UI.Monetary.Service.Controllers
         /// </summary>
         /// <param name="logger">A logger reference.</param>
         /// <param name="calculateBasePeriod">A domain logic reference.</param>
-        public MonetaryController(ILogger<MonetaryController> logger, ICalculateBasePeriod calculateBasePeriod)
+        public BasePeriodController(ILogger<BasePeriodController> logger, ICalculateBasePeriod calculateBasePeriod)
         {
             this.logger = logger;
             this.calculateBasePeriod = calculateBasePeriod;
         }
 
         /// <summary>
-        /// Calculate base period from initial claim date.
+        /// Calculate standard base period from initial claim date.
         /// </summary>
         /// <remarks>
         /// <b>Standard Calculation:</b><br />
@@ -52,6 +51,39 @@ namespace DWD.UI.Monetary.Service.Controllers
         /// The first full week of October was Sunday-Saturday 10/3-10/9 so
         /// that is when the quarter 4 would start for unemployment purposes. The week of 9/26-10/2
         /// would be considered to be apart of Q3.<br /><br />
+        /// </remarks>
+        /// <param name="initialClaimDate">The initial claim date in standard formats (MM/DD/YYYY, MM-DD-YYYY, YYYY-MM-DD, etc.).</param>
+        /// <returns>The calculated base period.</returns>
+        [SwaggerResponse((int)HttpStatusCode.OK, "OK", typeof(IBasePeriodDto), "application/json")]
+        [SwaggerResponse((int)HttpStatusCode.BadRequest, "Bad Initial Claim Date", typeof(ProblemDetails), "application/problem+json")]
+        [SwaggerResponse((int)HttpStatusCode.InternalServerError, "Internal Server Error", typeof(ProblemDetails), "application/problem+json")]
+        [Produces("application/json")]
+        [HttpGet]
+        [Route("GetStandardBasePeriodFromInitialClaimDate")]
+        public IActionResult GetStandardBasePeriodFromInitialClaimDate(DateTime initialClaimDate)
+        {
+            try
+            {
+                // Calculate the base period
+                var basePeriod = this.calculateBasePeriod.CalculateBasePeriodFromInitialClaimDate(initialClaimDate);
+
+                // Map from IBasePeriod to BasePeriodDto and return
+                var result = BasePeriodMapper.MapToDto(basePeriod.BasePeriodQuarters);
+                return this.Ok(result);
+            }
+            catch (ArgumentException argumentException)
+            {
+                // Log and return http 400
+                this.logger.LogError(argumentException, "Error calculating standard base period from initialClaimDate={0}", initialClaimDate);
+                var problem = this.Problem(argumentException.Message, null, 400);
+                return problem;
+            }
+        }
+
+        /// <summary>
+        /// Calculate alternate base period from initial claim date.
+        /// </summary>
+        /// <remarks>
         /// <b>Alternate Calculation:</b><br />
         /// The alternate base period will be the four most recently completed calendar quarters before the
         /// week you filed your initial claim application for a new benefit year<br /><br />
@@ -63,15 +95,14 @@ namespace DWD.UI.Monetary.Service.Controllers
         /// He does not qualify for a standard base period so the system will check for the alternate base period.
         /// </remarks>
         /// <param name="initialClaimDate">The initial claim date in standard formats (MM/DD/YYYY, MM-DD-YYYY, YYYY-MM-DD, etc.).</param>
-        /// <param name="useAltBase">Calculate the alternate base period?</param>
         /// <returns>The calculated base period.</returns>
         [SwaggerResponse((int)HttpStatusCode.OK, "OK", typeof(IBasePeriodDto), "application/json")]
         [SwaggerResponse((int)HttpStatusCode.BadRequest, "Bad Initial Claim Date", typeof(ProblemDetails), "application/problem+json")]
         [SwaggerResponse((int)HttpStatusCode.InternalServerError, "Internal Server Error", typeof(ProblemDetails), "application/problem+json")]
         [Produces("application/json")]
         [HttpGet]
-        [Route("GetBasePeriodFromInitialClaimDate")]
-        public IActionResult GetBasePeriodFromInitialClaimDate(DateTime initialClaimDate, bool useAltBase)
+        [Route("GetAlternateBasePeriodFromInitialClaimDate")]
+        public IActionResult GetAlternateBasePeriodFromInitialClaimDate(DateTime initialClaimDate)
         {
             try
             {
@@ -79,16 +110,17 @@ namespace DWD.UI.Monetary.Service.Controllers
                 var basePeriod = this.calculateBasePeriod.CalculateBasePeriodFromInitialClaimDate(initialClaimDate);
 
                 // Map from IBasePeriod to BasePeriodDto and return
-                var result = BasePeriodMapper.MapToDto(useAltBase ? basePeriod.AltBasePeriodQuarters : basePeriod.BasePeriodQuarters);
+                var result = BasePeriodMapper.MapToDto(basePeriod.AltBasePeriodQuarters);
                 return this.Ok(result);
             }
             catch (ArgumentException argumentException)
             {
                 // Log and return http 400
-                this.logger.LogError(argumentException, "Error calculating base period from initialClaimDate={0}", initialClaimDate);
+                this.logger.LogError(argumentException, "Error calculating alternate base period from initialClaimDate={0}", initialClaimDate);
                 var problem = this.Problem(argumentException.Message, null, 400);
                 return problem;
             }
         }
+
     }
 }
