227b840ef6d8ab5b90c06eec5a9ba4911847055e
dpuglielli <dpuglielli@flexion.us>
2021-12-21 06:06:53 -0600

    fixes #54 Tech debt on 48 s33 establishing a benefit year #51

--- DWD.UI.Monetary/DWD.UI.Monetary.Tests/Controllers/BenefitYearControllerTests.cs
+++ DWD.UI.Monetary/DWD.UI.Monetary.Tests/Controllers/BenefitYearControllerTests.cs
@@ -1,6 +1,7 @@
 namespace DWD.UI.Monetary.Tests.Controllers;
 
 using System;
+using System.Net;
 using Domain.BusinessEntities;
 using Domain.UseCases;
 using Microsoft.AspNetCore.Mvc;
@@ -14,17 +15,21 @@ public class BenefitYearControllerTests
     public void ShouldReturnValidBenefitYear()
     {
         // Arrange
-        var mockCalculateBenefitYear = new Mock<CalculateBenefitYear>();
-        var controller = new BenefitYearController(mockCalculateBenefitYear.Object);
+        var mockCalculateBenefitYear = new Mock<CalculateBenefitYear>().Object;
+        var controller = new BenefitYearController(mockCalculateBenefitYear);
 
         // Act
         var actionResult = controller.GetBenefitYearForRequestedDate(new DateTime(2021, 2, 1));
 
         // Assert
         Assert.NotNull(actionResult);
-        var okResult = actionResult.Result as OkObjectResult;
+        var okResult = Assert.IsType<OkObjectResult>(actionResult.Result);
+
         Assert.NotNull(okResult);
-        var okResultValue = okResult.Value as BenefitYear;
+        Assert.NotNull(okResult.StatusCode);
+        Assert.Equal((int)HttpStatusCode.OK, okResult.StatusCode.Value);
+
+        var okResultValue = Assert.IsType<BenefitYear>(okResult.Value);
         Assert.NotNull(okResultValue);
         Assert.Equal(52, okResultValue.BenefitWeeks);
         Assert.Equal(200, okResult.StatusCode);
