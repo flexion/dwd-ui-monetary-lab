4ae1678552ca57dc64fc7dfbda9e91f0c24bf15b
Jerry O''Brien <jerry.obrien@nelnet.net>
2021-12-22 11:45:47 -0600

    Removed PredicateBulder package

--- DWD.UI.Monetary/DWD.UI.PredicateBuilder.Tests/AnEntity.cs
+++
@@ -1,8 +0,0 @@
-namespace DWD.UI.PredicateBuilder.Tests;
-internal partial class AnEntity
-{
-    public int Id { get; set; }
-    public int Fk { get; set; }
-    public int P1 { get; set; }
-    public int P2 { get; set; }
-}
--- DWD.UI.Monetary/DWD.UI.PredicateBuilder.Tests/AnEntityQueryObject.cs
+++
@@ -1,7 +0,0 @@
-namespace DWD.UI.PredicateBuilder.Tests;
-
-internal class AnEntityQueryObject
-{
-    public int P1 { get; set; }
-    public int P2 { get; set; }
-}
